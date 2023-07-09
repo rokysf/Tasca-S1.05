a9db31a9c32f557d42df8300372735ca892ceb60
rokysf <135695631+rokysf@users.noreply.github.com>
2023-07-09 21:15:35 +0200

    Add files via upload

--- src/n1exercici4/LlegirTxt.java
+++ src/n1exercici4/LlegirTxt.java
@@ -1,30 +1,39 @@
 package n1exercici4;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Scanner;
+
 
 public class LlegirTxt {
 
-	private static Scanner sc = new Scanner(System.in);
-	
 	public static void main(String[] args) {
 		
-		String ruta = sc.nextLine();
+		String rutaGuardar = "c:";
+		String rutaLlegir ="src" + File.separator + "n1exercici4" + File.separator + "prova.txt";
 			
 		try {
-		llegirTxt(ruta);
+			guardarArbreTxt(rutaGuardar);
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
+	
+		try {
+			llegirTxt(rutaLlegir);
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	
 	}
 
-	public static void llegirTxt(String ruta) throws IOException {
-		FileReader fitxer = new FileReader(ruta);
+	public static void guardarArbreTxt(String ruta) throws IOException {
+		File directori = new File(ruta);
 		File[] llista = directori.listFiles();
-		FileWriter escriureTxt = new FileWriter("src" + File.separator + "n1exercici3" + File.separator + "prova.txt");
+		BufferedWriter escriureTxt = new BufferedWriter(new FileWriter("src" + File.separator + "n1exercici4" + File.separator + "prova.txt"));
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
 		for (int i = 0; i < llista.length; i++) {
@@ -37,8 +46,18 @@ public class LlegirTxt {
 				escriureTxt.write(String.format("%s (%s) - %s", arxiu.getName(), arxiu.isDirectory() ? "D" : "F", sdf.format(arxiu.lastModified())+"\n"));
 				guardarArbreTxt(arxiu.getAbsolutePath());
 				}
-			
 		}		
 		escriureTxt.close();
 	}
+
+	public static void llegirTxt(String ruta) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(ruta));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        reader.close();
+	}
+	
 }
